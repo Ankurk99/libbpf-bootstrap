d945efebe39aed9e3f9fa12919dc25de500fb5be
Rahul Jadhav <nyrahul@gmail.com>
2022-02-15 15:50:21 +0530

    added security_bprm_check sample code.
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

---
+++ examples/c/pspath.bpf.c
@@ -0,0 +1,190 @@
+// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+/* Copyright (c) 2021 Sartura */
+
+/* CREDITS: code heavily based on Tracee's implementation */
+
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+
+/*
+ * Limitations of the code:
+ * 1. Can only be used for the file path matching
+ * 2. Limit on first pass:12chars second pass:64chars third pass:12chars
+ *
+ */
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+#if 0
+SEC("kprobe/do_execve")
+int BPF_KPROBE(do_execve, const char *name,
+		const char *const *__argv,
+		const char *const *__envp)
+{
+	pid_t pid;
+	char comm[64];
+	char filename[256];
+
+	pid = bpf_get_current_pid_tgid() >> 32;
+	bpf_get_current_comm(&comm, sizeof(comm));
+	bpf_probe_read_kernel_str(filename, sizeof(filename), (void *)name);
+	//bpf_probe_read_user_str(filename, sizeof(filename), (const char*)ctx->args[0]);
+	bpf_printk("=------KPROBE ENTRY pid=%d, comm=%s\n", pid, filename);
+	return 0;
+}
+#endif
+
+SEC("tracepoint/syscalls/sys_enter_execve")
+int tracepoint__syscalls__sys_enter_execve(struct trace_event_raw_sys_enter* ctx)
+{
+	char filename[128];
+
+	bpf_probe_read_user_str(filename, sizeof(filename), (const char*)ctx->args[0]);
+	bpf_printk("=------process=%s\n", filename);
+	return 0;
+}
+
+#define READ_KERN(ptr)                                                  \
+    ({                                                                  \
+        typeof(ptr) _val;                                               \
+        __builtin_memset((void *)&_val, 0, sizeof(_val));               \
+        bpf_core_read((void *)&_val, sizeof(_val), &ptr);               \
+        _val;                                                           \
+    })
+
+static __always_inline dev_t get_dev_from_file(struct file *file)
+{   
+    struct inode *f_inode = READ_KERN(file->f_inode);
+    struct super_block *i_sb = READ_KERN(f_inode->i_sb);
+    return READ_KERN(i_sb->s_dev);
+}   
+
+static __always_inline unsigned long get_inode_nr_from_file(struct file *file)
+{
+    struct inode *f_inode = READ_KERN(file->f_inode);
+    return READ_KERN(f_inode->i_ino);
+}
+
+static __always_inline struct qstr get_d_name_from_dentry(struct dentry *dentry)
+{
+    return READ_KERN(dentry->d_name);
+}
+
+static __always_inline struct file* get_file_ptr_from_bprm(struct linux_binprm *bprm)
+{
+    return READ_KERN(bprm->file);
+}
+
+#define GET_FIELD_ADDR(field) __builtin_preserve_access_index(&field)
+
+static __always_inline struct dentry* get_mnt_root_ptr_from_vfsmnt(struct vfsmount *vfsmnt)
+{
+	return READ_KERN(vfsmnt->mnt_root);
+}
+
+static __always_inline struct dentry* get_d_parent_ptr_from_dentry(struct dentry *dentry)
+{
+	return READ_KERN(dentry->d_parent);
+}
+
+static inline struct mount *real_mount(struct vfsmount *mnt)
+{
+    return container_of(mnt, struct mount, mnt);
+}
+
+#define MAX_PERCPU_BUFSIZE 256
+#define MAX_STRING_SIZE 128
+#define MAX_PATH_SZ 32
+#define MAX_PATH_COMPONENTS 10
+
+static inline int copystr(char *dst, char *src, int len) {
+	int c=0;
+#pragma unroll
+	for (;;c++) {
+		if(c>=len) break;
+		if(!src[c]) break;
+		dst[c] = src[c];
+	}
+	return c;
+}
+
+static __always_inline void get_path_str(struct path *path)//, char *filename, int max_len)
+{
+    struct path f_path;
+    bpf_probe_read(&f_path, sizeof(struct path), path);
+    char slash = '/';
+    int zero = 0;
+    struct dentry *dentry = f_path.dentry;
+    struct vfsmount *vfsmnt = f_path.mnt;
+    struct mount *mnt_parent_p;
+
+    struct mount *mnt_p = real_mount(vfsmnt);
+    bpf_probe_read(&mnt_parent_p, sizeof(struct mount*), &mnt_p->mnt_parent);
+
+    struct dentry *mnt_root;
+    struct dentry *d_parent;
+    struct qstr d_name;
+    unsigned int len;
+    int sz;
+	char pc[MAX_PATH_COMPONENTS][MAX_PATH_SZ];
+	char filename[128];
+	int off = 0;
+	int pcidx = MAX_PATH_COMPONENTS - 1;
+
+    #pragma unroll
+    for (int i = 0; i < MAX_PATH_COMPONENTS; i++) {
+        mnt_root = get_mnt_root_ptr_from_vfsmnt(vfsmnt);
+        d_parent = get_d_parent_ptr_from_dentry(dentry);
+        if (dentry == mnt_root || dentry == d_parent) {
+            if (dentry != mnt_root) {
+                // We reached root, but not mount root - escaped?
+                break;
+            }
+            if (mnt_p != mnt_parent_p) {
+                // We reached root, but not global root - continue with mount point path
+                bpf_probe_read(&dentry, sizeof(struct dentry*), &mnt_p->mnt_mountpoint);
+                bpf_probe_read(&mnt_p, sizeof(struct mount*), &mnt_p->mnt_parent);
+                bpf_probe_read(&mnt_parent_p, sizeof(struct mount*), &mnt_p->mnt_parent);
+                vfsmnt = &mnt_p->mnt;
+                continue;
+            }
+            // Global root - path fully parsed
+            break;
+        }
+        // Add this dentry name to path
+        d_name = get_d_name_from_dentry(dentry);
+        len = (d_name.len+1) & (MAX_PATH_SZ-1);
+
+		if (pcidx < 0) {
+			break;
+		}
+
+        // Is string buffer big enough for dentry name?
+        sz = 0;
+		if (len < sizeof(pc[pcidx])) {
+            bpf_probe_read(pc[pcidx], 1, &slash);
+			sz = bpf_probe_read_str(&pc[pcidx][1], sizeof(pc[pcidx]), (void *)d_name.name);
+			if(sz > 1)
+				pcidx--;
+			else
+				break;
+		}
+        dentry = d_parent;
+    }
+#pragma unroll
+	for (pcidx++;pcidx<MAX_PATH_COMPONENTS;pcidx++) {
+		bpf_printk("part=%s\n", pc[pcidx]);
+	}
+}
+
+SEC("kprobe/security_bprm_check")
+int BPF_KPROBE(trace_security_bprm_check)
+{
+    struct linux_binprm *bprm = (struct linux_binprm *)PT_REGS_PARM1(ctx);
+    struct file* file = get_file_ptr_from_bprm(bprm);
+    get_path_str(GET_FIELD_ADDR(file->f_path));
+    return 0;
+}
+
